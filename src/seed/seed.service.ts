28c42163df2fcaa0b1c0415a2c712a62268ce920
John Satur <animashorse3@yahoo.com.mx>
2024-11-30 23:57:20 -0600

    Subiendo a railway

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -2,24 +2,38 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { lastValueFrom } from 'rxjs';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    @InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>,
+  ) {}
   async executeSeed() {
+    await this.pokemonModel.deleteMany({});
+
     const { data } = await lastValueFrom(
       this.httpService.get<PokeResponse>(
-        'https://pokeapi.co/api/v2/pokemon?limit=10',
+        'https://pokeapi.co/api/v2/pokemon?limit=650',
       ),
     );
+
+    const pokemonToInsert: { name: string; no: number }[] = [];
 
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
 
-      console.log(name, no);
+      // await this.pokemonModel.create({ name, no });
+
+      pokemonToInsert.push({ name, no });
     });
 
-    return data.results;
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+    return 'SEED executed!';
   }
 }
